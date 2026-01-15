36faf562a6897214fcef54bbe8d74755f02e4b53
JanThorbecke <janth@xs4all.nl>
2026-01-15 10:15:04 +0100

    Intial set-up

---
+++ Handler/handler.c
@@ -0,0 +1,28 @@
+// Adapted from source: http://www.firmcodes.com/signals-in-linux/
+// Accessed: 2019-07-09
+ 
+// Example shows a custom handler for SIGINT
+// but the handler reverts to default action for future signals.
+// Thus, first ctrl+c will allow program to continue 
+// and second ctrl+c will terminate the program.
+ 
+#include <unistd.h>
+#include <stdio.h>
+#include <signal.h>
+ 
+void sig_handler1(int num)
+{
+	printf("You are here and catched signal: %d\n", num);	
+	signal(SIGINT, SIG_DFL);
+}
+ 
+int main()
+{
+	signal(SIGINT, sig_handler1);
+	while(1)
+	{
+		printf("Hello \n");
+		sleep(2);
+	}
+}
+ 
---
+++ Hello/hello.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+
+int main(void) {
+    int i = 0;
+    int j = 1;
+
+    i = 9;
+    printf("Hello World!\n");
+    printf("i=%d\n",i);
+    i++;
+    printf("i is now =%d\n",i);
+
+    return 0;
+}
